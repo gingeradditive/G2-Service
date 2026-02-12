29de34427eeb6fe04e61b74a683bd0d581fb84b1
Jack_up <36533859+GiacomoGuaresi@users.noreply.github.com>
2026-02-12 17:13:47 +0100

    chore: remove Klipperscreen

--- install.sh
+++ install.sh
@@ -47,21 +47,6 @@ sudo ln -s "$HOME/G1-Configs/Configs/G1-Configs" "$SYMBOLIC_LINK_DESTINATION"
 sudo chown -h pi:pi "$SYMBOLIC_LINK_DESTINATION"
 echo "Symbolic link created for Ginger Configs"
 
-echo "Installing klipperscreen style"
-ORIGIN_DIR="$HOME/G1-Configs/Styles/klipperscreen-ginger/"
-DESTINATION_DIR="$HOME/KlipperScreen/styles/klipperscreen-ginger"
-if [ -L "$DESTINATION_DIR" ]; then
-    echo "Symbolic link already exists, removing it."
-    rm "$DESTINATION_DIR"
-fi
-ln -s "$ORIGIN_DIR" "$DESTINATION_DIR"
-
-if [ $? -eq 0 ]; then
-    echo "Symbolic link created successfully!"
-else
-    echo "Error creating symbolic link."
-fi
-
 echo "Install mainsail style"
 mkdir /home/pi/printer_data/config/.theme
 ln -sf $HOME/G1-Configs/Styles/mainsail-ginger/*.* "/home/pi/printer_data/config/.theme/"
